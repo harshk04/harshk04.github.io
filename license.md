5856c0b2b09bfe920179ebd7c944d947c7027188
harshk04 <kumawatharsh2004@gmail.com>
2023-06-22 11:36:44 +0530

    commit

--- license.md
+++ license.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2023 Pushpak Kumawat
+Copyright (c) 2023 Harsh Kumawat
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- projects/script.js
+++ projects/script.js
@@ -20,12 +20,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Pushpak";
-            $("#favicon").attr("href", "/assets/images/favicon1.png");
+            document.title = "Projects | Portfolio Harsh";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "/assets/images/favicon1.png");
+            $("#favicon").attr("href", "/assets/images/favicon.png");
         }
     });
 
