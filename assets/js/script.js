4d8fb4ab7d22348abe0f98d762f34463c1dde7d4
harshk04 <kumawatharsh2004@gmail.com>
2024-06-24 23:53:48 +0530

    Minor Chnges

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["C++ development","Python Development","Machine Learning","Neural Networking" ],
+    strings: ["Data Science","Generative AI","Machine Learning","Python Development","Neural Networking","Computer Vision","C++ development" ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
